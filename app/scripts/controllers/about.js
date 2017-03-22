01c7959167f5ba6bae6de4ca65050dadcdcd37fc
admin <jsmorgan6@gmail.com>
2017-03-22 05:25:12 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chowSauceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chowSauceApp
+ */
+angular.module('chowSauceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chowSauceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chowSauceApp
+ */
+angular.module('chowSauceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
